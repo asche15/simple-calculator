06817726e0627b0a1e307d22ca431fdad29d6071
Aschalew <aschalew.tesfaye15@gmail.com>
2025-08-02 13:23:08 +0300

    initial commit

---
+++ calculator.py
@@ -0,0 +1,14 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the seond number: "))
+operation = input("Enter the operation: ")
+
+if operation == "add":
+  print(first_number + second_number)
+
+elif operation == "multiply":
+    print(first_number * second_number)
+
+elif operation == "divide":
+     print(first_number / second_number)
+
+
